5519a303fb2b8838a102277b7b1abbaef29af146
abdullah <abddiucse@gmail.com>
2024-10-16 00:31:23 +0600

    Set room database for local storage.

---
+++ app/src/main/java/com/example/newspaper/db/Converters.kt
@@ -0,0 +1,20 @@
+package com.example.newspaper.db
+
+import androidx.room.TypeConverter
+import com.example.newspaper.network.Source
+
+class Converters {
+    // Convert a Source object to a String
+    @TypeConverter
+    fun fromSource(source: Source): String {
+        return source.name
+    }
+
+    // Convert a String to a Source object
+    @TypeConverter
+    fun toSource(name: String): Source {
+        return Source(name, name)
+
+    }
+}
+
