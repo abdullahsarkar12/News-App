dfbc5f7d7e3534e41d3b0b34ba05a1e7c32246b2
abdullah <abddiucse@gmail.com>
2024-10-15 22:41:03 +0600

    Retrofit initialization.

---
+++ app/src/main/java/com/example/newspaper/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.newspaper.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "e3865bb2aadf436eba3b9d7e09b8ed07"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
