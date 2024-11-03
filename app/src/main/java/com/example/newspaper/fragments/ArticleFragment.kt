ffb1d7fc46ccfeaaeb2c51888be9f343f42f1aae
abdullah <abddiucse@gmail.com>
2024-11-03 12:04:40 +0600

    Create base skeleton of all fragments.

--- app/src/main/java/com/example/newspaper/fragments/ArticleFragment.kt
+++ app/src/main/java/com/example/newspaper/fragments/ArticleFragment.kt
@@ -5,12 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.newspaper.NewsActivity
 import com.example.newspaper.R
+import com.example.newspaper.viewmodels.NewsViewModel
 
 class ArticleFragment : Fragment() {
 
+    private lateinit var viewModel: NewsViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        viewModel = (activity as NewsActivity).viewModel
     }
 
     override fun onCreateView(
--- app/src/main/java/com/example/newspaper/fragments/BreakingNewsFragment.kt
+++ app/src/main/java/com/example/newspaper/fragments/BreakingNewsFragment.kt
@@ -1,24 +1,77 @@
 package com.example.newspaper.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.newspaper.R
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.newspaper.NewsActivity
+import com.example.newspaper.adapters.NewsAdapter
+import com.example.newspaper.databinding.FragmentBreakingNewsBinding
+import com.example.newspaper.util.Resource
+import com.example.newspaper.viewmodels.NewsViewModel
 
 class BreakingNewsFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private var _binding: FragmentBreakingNewsBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var newsAdapter: NewsAdapter
+
+    private lateinit var viewModel: NewsViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_breaking_news, container, false)
+    ): View {
+        // Initialize ViewBinding
+        _binding = FragmentBreakingNewsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // Initialize ViewModel
+        viewModel = (activity as NewsActivity).viewModel
+        setupRecyclerView()
+
+        viewModel.breakingNews.observe(viewLifecycleOwner, Observer { response->
+            when(response){
+                is Resource.Success ->{
+                    hideProgressBar()
+                    response.data.let {
+                        newsResponse ->
+                        newsAdapter.submitList(newsResponse?.articles)
+                    }
+                }
+                is Resource.Error -> {
+                    hideProgressBar()
+                    response.message?.let { message ->
+                        // Handle error message
+                    }
+                }
+                is Resource.Loading -> {
+                    showProgressBar()
+                }
+            }
+        })
+    }
+
+    private fun hideProgressBar(){
+
+    }
+
+    private fun showProgressBar(){
+
+    }
+
+    private fun setupRecyclerView(){
+        newsAdapter = NewsAdapter()
+        binding.rvBreakingNews.apply {
+            adapter = newsAdapter
+            layoutManager = LinearLayoutManager(activity)
+        }
     }
 
 }
