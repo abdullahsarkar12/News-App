86c7c1aa22f484def6a2a5ead29db5b12bfa7075
abdullah <abddiucse@gmail.com>
2024-12-04 20:24:41 +0600

    Recheck gradle and toml file.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,12 +58,20 @@ dependencies {
     implementation (libs.androidx.lifecycle.viewmodel.ktx)
 
     // Room
-    implementation(libs.androidx.room.runtime)
+    /*implementation(libs.androidx.room.runtime)
     annotationProcessor(libs.androidx.room.compiler)
     // Room KTX for Coroutine support
     //implementation(libs.androidx.room.ktx)
-    implementation ("androidx.room:room-ktx:2.2.1")
-    //kapt ("androidx.room:room-compiler:2.2.1")
+    implementation ("androidx.room:room-ktx:2.6.1")
+    ksp(libs.androidx.room.compiler)
+    //kapt ("androidx.room:room-compiler:2.2.1")*/
+
+    implementation("androidx.room:room-runtime:2.6.1")
+    annotationProcessor("androidx.room:room-compiler:2.6.1")
+    // To use Kotlin annotation processing tool (kapt)
+    ksp("androidx.room:room-compiler:2.6.1")
+    // optional - Kotlin Extensions and Coroutines support for Room
+    implementation("androidx.room:room-ktx:$2.6.1")
 
     // Coroutine
     implementation(libs.kotlinx.coroutines.android)
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
-    id("com.google.devtools.ksp") version "1.9.25-1.0.20" apply false
+    id("com.google.devtools.ksp") version "1.9.25-1.0.20" apply true
 }
 
 buildscript {
